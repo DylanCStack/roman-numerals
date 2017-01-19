20a87b69674bcd5b09c268d91a321b02ad878c7c
David Quisenberry and Dylan Stackhouse <david@>
2017-01-19 09:05:59 -0800

    convert decimals with direct roman numeral representation

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,46 @@
-$(function(){
+var numerals = ["I","V","X","L","C","D","M"];
 
+var getRoman = function(input) {
+  switch (input) {
+    case "1":
+      return "I";
+      break;
+    case "5":
+      return "V";
+      break;
+    case "10":
+      return "X";
+      break;
+    case "50":
+      return "L";
+      break;
+    case "100":
+      return "C";
+      break;
+    case "500":
+      return "D"
+      break;
+    case "1000":
+      return "M";
+      break;
+
+    default:
+      break;
+
+  }
+
+
+};
+
+
+$(function(){
   $("form").submit(function(event){
     event.preventDefault();
-    console.log("button clicked");
     var input = $("#decimal").val();
 
 
-    var output = "Romans go home!";
+
+    var output = getRoman(input);
     $("#output").text(output);
   });
 
