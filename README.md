b71e59c3ae92e768f08bd11a090cc25168bd199f
David Quisenberry and Dylan Stackhouse <david@>
2017-01-19 09:34:19 -0800

    add business logic to take multiple digit input with direct representation

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Website for converting numbers into roman numerals
 Clone repo.  Open index.html.
 ## Specs
 1. Enter a decimal that has a direct roman numeral representation. Input: 1 Output: I
-2. Enter a decimal with multiple digits all of which have a direct roman numeral representation. Input: 25 Output: XXV
+2. Enter a decimal with multiple digits all of which have a direct roman numeral representation. Input: 55 Output: LV
 3. Enter a decimal which requires only roman numeral addition to represent. Input: 2 Output: II
 4. Enter a multiple digit decimal number which requires only roman numeral addition to represent. Input: 16 Output: XVI
 5. Enter a decimal which only requires a singular subtraction to represent in roman numerals. Input: 4 Output: IV
--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,38 @@
 var numerals = ["I","V","X","L","C","D","M"];
 
+var translate = function(num) {
+  num.split("");
+  var outputStr = "";
+  for (var i = 0; i < num.length; i++){
+    // always will start with the singles digits and works towards tens, hundreds, thousands
+    var temp = getRoman(parseInt(num[num.length-1-i]) * Math.pow(10,i));
+    // concat by adding temp (higher place) to the left of the lower places
+    outputStr = temp + outputStr;
+  }
+  return outputStr;
+}
+
 var getRoman = function(input) {
   switch (input) {
-    case "1":
+    case 1:
       return "I";
       break;
-    case "5":
+    case 5:
       return "V";
       break;
-    case "10":
+    case 10:
       return "X";
       break;
-    case "50":
+    case 50:
       return "L";
       break;
-    case "100":
+    case 100:
       return "C";
       break;
-    case "500":
+    case 500:
       return "D"
       break;
-    case "1000":
+    case 1000:
       return "M";
       break;
 
@@ -40,7 +52,7 @@ $(function(){
 
 
 
-    var output = getRoman(input);
+    var output = translate(input);
     $("#output").text(output);
   });
 
